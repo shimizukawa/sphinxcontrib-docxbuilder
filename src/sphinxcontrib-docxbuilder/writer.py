34be1e8016e527a5064cefd63a94b6d7df005881
shimizukawa <devnull@localhost>
2010-09-23 22:07:00 +0900

    * print debug out replace with logging module.

--- src/sphinxcontrib-docxbuilder/writer.py
+++ src/sphinxcontrib-docxbuilder/writer.py
@@ -20,21 +20,28 @@ from sphinx.locale import admonitionlabels, versionlabels, _
 import docx
 import sys
 
+import logging
+logging.basicConfig(filename='docx.log', filemode='w', level=logging.INFO,
+        format="%(asctime)-15s  %(message)s")
+logger = logging.getLogger('docx')
+
 
 def dprint(_func=None, **kw):
     f = sys._getframe(1)
     if kw:
         text = ', '.join('%s = %s' % (k,v) for k,v in kw.items())
     else:
-        text = dict((k,v) for k,v in f.f_locals.items() if k != 'self')
+        try:
+            text = dict((k,repr(v)) for k,v in f.f_locals.items()
+                        if k != 'self')
+            text = unicode(text)
+        except:
+            text = ''
 
     if _func is None:
         _func = f.f_code.co_name
 
-    try:
-        print >>sys.stderr, '###', _func, text
-    except:
-        print >>sys.stderr, ''
+    logger.info(' '.join([_func, text]))
 
 
 class DocxWriter(writers.Writer):
